dd4858da1bb1c50e42851f53695c14e2fe11e341
KrissiBT <minecraft.man96@gmail.com>
2021-09-29 17:55:26 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 pip3 install -r requirements.txt
-python3 __main__.py --url http://hash.kristofer.is/api/server.php
+python3 __main__.py --url http://hash.kristofer.is/api/server.php --voucher unSecureVoucher
